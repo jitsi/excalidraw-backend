eb669aade6ba7f1497302e277b870e32c170c866
Yash <139779840+yashop7@users.noreply.github.com>
2025-05-31 11:30:07 +0530

    Socket upgrade V4 with prometheus support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import debug from 'debug';
+import debug from "debug";
 import dotenv from 'dotenv';
-import express from 'express';
-import http from 'http';
-import socketIO, { Socket } from 'socket.io';
-import * as prometheus from 'socket.io-prometheus-metrics';
+import express from "express";
+import http from "http";
+import { Server as SocketIO } from "socket.io";
+import { metrics } from './prometheus';
 
-const serverDebug = debug('server');
+type UserToFollow = {
+  socketId: string;
+  username: string;
+};
+type OnUserFollowedPayload = {
+  userToFollow: UserToFollow;
+  action: "FOLLOW" | "UNFOLLOW";
+};
+
+const serverDebug = debug("server");
+const ioDebug = debug("io");
+const socketDebug = debug("socket");
 
 dotenv.config(
   process.env.NODE_ENV === 'development'
@@ -14,12 +25,11 @@ dotenv.config(
 );
 
 const app = express();
-const port = process.env.PORT || 80; // default port to listen
-const users: Socket[] = [];
-const userLimit = Number(process.env.USER_LIMIT) || Infinity;
+const port = process.env.PORT || (process.env.NODE_ENV !== "development" ? 80 : 3002); // default port to listen
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.send('Excalidraw backend is up :)');
+app.get("/", (req, res) => {
+  res.send("Excalidraw backend is up :)");
 });
 
 const server = http.createServer(app);
@@ -28,99 +38,129 @@ server.listen(port, () => {
     serverDebug(`listening on port: ${port}`);
 });
 
-const io = socketIO(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            'Access-Control-Allow-Origin': req.header?.origin ?? 'https://meet.jit.si',
-            'Access-Control-Allow-Credentials': true
-        };
-
-        res.writeHead(200, headers);
-        res.end();
+try {
+  const io = new SocketIO(server, {
+    transports: ["websocket", "polling"],
+    cors: {
+      allowedHeaders: ["Content-Type", "Authorization"],
+      origin: process.env.CORS_ORIGIN || "*",
+      credentials: true,
     },
+    allowEIO3: true,
     maxHttpBufferSize: 20e6,
     pingTimeout: 60000
 });
 
 // listens on host:9090/metrics
-prometheus.metrics(io, {
+metrics(io, {
     collectDefaultMetrics: true
 });
 
-io.on('connection', socket => {
+  io.on("connection", (socket) => {
     serverDebug(`connection established! ${socket.conn.request.url}`);
-    io.to(`${socket.id}`).emit('init-room');
-    socket.on('join-room', roomID => {
-        serverDebug(`${socket.id} has joined ${roomID} for url ${socket.conn.request.url}`);
-        socket.join(roomID);
+    ioDebug("connection established!");
+    io.to(`${socket.id}`).emit("init-room");
+    socket.on("join-room", async (roomID) => {
+      serverDebug(`${socket.id} has joined ${roomID} for url ${socket.conn.request.url}`);
+      await socket.join(roomID);
+      const sockets = await io.in(roomID).fetchSockets();
+      if (sockets.length <= 1) {
+        io.to(`${socket.id}`).emit("first-in-room");
+      } else {
+        socketDebug(`${socket.id} new-user emitted to room ${roomID}`);
+        socket.broadcast.to(roomID).emit("new-user", socket.id);
+      }
 
-        users.push(socket);
-        socket.on('close', () => {
-            users.splice(users.indexOf(socket), 1);
-        });
-
-        const clients = Object.keys(io.sockets.adapter.rooms[roomID].sockets);
-
-        if (clients.length > userLimit) {
-            clients.forEach((clientKey: string) => {
-                const clientSocket = io.sockets.connected[clientKey];
-
-                serverDebug(`${clientSocket} has left the ${roomID} room because the user limit was reached.`);
-                clientSocket.leave(roomID);
-            });
-
-            return;
-        }
-
-        if (io.sockets.adapter.rooms[roomID].length <= 1) {
-            io.to(`${socket.id}`).emit('first-in-room');
-        } else {
-            socket.broadcast.to(roomID).emit('new-user', socket.id);
-        }
-        io.in(roomID).emit(
-            'room-user-change',
-            Object.keys(io.sockets.adapter.rooms[roomID].sockets)
-        );
+      io.in(roomID).emit(
+        "room-user-change",
+        sockets.map((socket) => socket.id),
+      );
     });
 
     socket.on(
-    'server-broadcast',
-        (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-            socket.broadcast.to(roomID).emit('client-broadcast', encryptedData, iv);
-        }
+      "server-broadcast",
+      (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+        socketDebug(`${socket.id} sends update to ${roomID}`);
+        socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
+      },
     );
 
     socket.on(
-    'server-volatile-broadcast',
-    (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+      "server-volatile-broadcast",
+      (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+        socketDebug(`${socket.id} sends volatile update to ${roomID}`);
         socket.volatile.broadcast
-        .to(roomID)
-        .emit('client-broadcast', encryptedData, iv);
-    }
+          .to(roomID)
+          .emit("client-broadcast", encryptedData, iv);
+      },
     );
+    
+    socket.on("disconnecting", async () => {
+      socketDebug(`${socket.id} has disconnected`);
+      for (const roomID of Array.from(socket.rooms)) {
+        const otherClients = (await io.in(roomID).fetchSockets()).filter(
+          (_socket) => _socket.id !== socket.id,
+        );
 
-    socket.on('disconnecting', () => {
-        const rooms = io.sockets.adapter.rooms;
+        const isFollowRoom = roomID.startsWith("follow@");
 
-        for (const roomID of Object.keys(socket.rooms)) {
-            const clients = Object.keys(rooms[roomID].sockets).filter(id => id !== socket.id);
-
-            if (roomID !== socket.id) {
-                socket.to(roomID).emit('user has left', socket.id);
-            }
-
-            if (clients.length > 0) {
-                socket.broadcast.to(roomID).emit('room-user-change', clients);
-            }
+        if (!isFollowRoom && otherClients.length > 0) {
+          socket.broadcast.to(roomID).emit(
+            "room-user-change",
+            otherClients.map((socket) => socket.id),
+          );
         }
+
+        if (isFollowRoom && otherClients.length === 0) {
+          const socketId = roomID.replace("follow@", "");
+          io.to(socketId).emit("broadcast-unfollow");
+        }
+      }
     });
 
-    socket.on('disconnect', (reason, details) => {
+    socket.on("user-follow", async (payload: OnUserFollowedPayload) => {
+      const roomID = `follow@${payload.userToFollow.socketId}`;
+
+      switch (payload.action) {
+        case "FOLLOW": {
+          await socket.join(roomID);
+
+          const sockets = await io.in(roomID).fetchSockets();
+          const followedBy = sockets.map((socket) => socket.id);
+
+          io.to(payload.userToFollow.socketId).emit(
+            "user-follow-room-change",
+            followedBy,
+          );
+
+          break;
+        }
+        case "UNFOLLOW": {
+          await socket.leave(roomID);
+
+          const sockets = await io.in(roomID).fetchSockets();
+          const followedBy = sockets.map((socket) => socket.id);
+
+          io.to(payload.userToFollow.socketId).emit(
+            "user-follow-room-change",
+            followedBy,
+          );
+
+          break;
+        }
+      }
+    });
+
+    socket.on("disconnect", (reason, details) => {
         serverDebug(
             `${socket.id} was disconnected from url ${socket.conn.request.url} for the following reason: ${reason}
             ${JSON.stringify(details)}`
         );
         socket.removeAllListeners();
+        socket.disconnect();
     });
-});
+  });
+} catch (error) {
+  console.error(error);
+}
+
